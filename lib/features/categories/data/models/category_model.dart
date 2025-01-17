f23494861da49252374224fbee8a67332502382a
bryann54 <alfredbryann545@gmail.com>
2025-01-17 18:22:19 +0300

    brands from firebase

--- lib/features/categories/data/models/category_model.dart
+++ lib/features/categories/data/models/category_model.dart
@@ -3,5 +3,37 @@ class CategoryModel {
   final String name;
   final String imageUrl;
 
-  CategoryModel({required this.id, required this.name, required this.imageUrl});
+  CategoryModel({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+  });
+
+  // JSON serialization methods
+  factory CategoryModel.fromJson(Map<String, dynamic> json) {
+    return CategoryModel(
+      id: json['id'],
+      name: json['name'],
+      imageUrl: json['imageUrl'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'imageUrl': imageUrl,
+    };
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CategoryModel &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          name == other.name;
+
+  @override
+  int get hashCode => id.hashCode ^ name.hashCode;
 }

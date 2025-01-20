7fcea19a581d1946107b004645b567f3c22a444b
bryann54 <alfredbryann545@gmail.com>
2025-01-20 10:53:28 +0300

    test

--- lib/features/analytics/presentation/pages/analytics_screen.dart
+++ lib/features/analytics/presentation/pages/analytics_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quickpourmerchant/features/analytics/presentation/bloc/analytics_bloc.dart';
-import 'package:quickpourmerchant/features/analytics/presentation/widgets/stat_card.dart';
-import 'package:quickpourmerchant/features/analytics/presentation/widgets/workflow_card.dart';
 import 'package:quickpourmerchant/features/auth/data/repositories/auth_repository.dart';
 import 'package:quickpourmerchant/features/auth/domain/usecases/auth_usecases.dart';
 
 class AnalyticsScreen extends StatefulWidget {
+  const AnalyticsScreen({super.key});
+
   @override
-  _AnalyticsScreenState createState() => _AnalyticsScreenState();
+  State<AnalyticsScreen> createState() => _AnalyticsScreenState();
 }
 
 class _AnalyticsScreenState extends State<AnalyticsScreen> {
@@ -22,12 +22,16 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
   }
 
   Future<void> _fetchUserName() async {
+    // Fetch the username logic (as per your existing code)
     final authRepository = AuthRepository();
     final authUseCases = AuthUseCases(authRepository: authRepository);
     final user = await authUseCases.getCurrentUserDetails();
     setState(() {
       userName = user?.storeName ?? '';
     });
+
+    // Fetch analytics data once the user name is fetched
+    context.read<AnalyticsBloc>().add(FetchAnalyticsData());
   }
 
   @override
@@ -57,6 +61,7 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
               ),
             ),
             const SizedBox(height: 16),
+            // Time Filter Buttons
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: ['7 Days', '30 Days', '90 Days', 'Custom']
@@ -79,77 +84,208 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
                   .toList(),
             ),
             const SizedBox(height: 20),
-            BlocBuilder<AnalyticsBloc, AnalyticsState>(
-              builder: (context, state) {
-                if (state is AnalyticsLoading) {
-                  return const Expanded(
-                    child: Center(child: CircularProgressIndicator()),
-                  );
-                } else if (state is AnalyticsError) {
-                  return Expanded(
-                    child: Center(child: Text(state.message)),
-                  );
-                } else if (state is AnalyticsLoaded) {
-                  return Expanded(
-                    child: Column(
+            // Display analytics data
+            Expanded(
+              child: BlocBuilder<AnalyticsBloc, AnalyticsState>(
+                builder: (context, state) {
+                  if (state is AnalyticsLoading) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (state is AnalyticsError) {
+                    return Center(child: Text(state.message));
+                  } else if (state is AnalyticsLoaded) {
+                    return GridView.count(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
+                      childAspectRatio: 1.4,
+
                       children: [
-                        Expanded(
-                          child: GridView.count(
-                            crossAxisCount: 2,
-                            crossAxisSpacing: 16,
-                            mainAxisSpacing: 16,
-                            childAspectRatio: 1.78,
-                            children: [
-                              StatCard(
-                                title: 'In Stock',
-                                value: state.stockCount.toString(),
-                                icon: Icons.inventory,
-                                color: theme.colorScheme.primary,
-                              ),
-                              StatCard(
-                                title: 'Orders',
-                                value: state.ordersCount.toString(),
-                                icon: Icons.shopping_cart,
-                                color: theme.colorScheme.secondary,
-                                percentage: '13%',
-                                isIncrease: true,
-                              ),
-                              StatCard(
-                                title: 'Feedback',
-                                value: state.feedbackCount.toString(),
-                                icon: Icons.thumb_up,
-                                color: theme.colorScheme.tertiary,
-                                percentage: '13%',
-                                isIncrease: false,
-                              ),
-                              StatCard(
-                                title: 'Reviews',
-                                value: '2.6k',
-                                icon: Icons.reviews,
-                                color: theme.colorScheme.error,
-                                percentage: '1.2%',
-                                isIncrease: true,
-                                extraInfo: '233 ASINs',
-                              ),
-                            ],
-                          ),
+                        _buildStatCard(
+                          context,
+                          'In Stock',
+                          state.stockCount.toString(),
+                          Icons.inventory,
+                          theme.colorScheme.primary,
                         ),
-                        const SizedBox(height: 16),
-                        WorkflowCard(
-                          onCreateWorkflow: () {
-                            // Handle creating a workflow
-                          },
+                        _buildStatCard(
+                          context,
+                          'Orders',
+                          state.ordersCount.toString(),
+                          Icons.shopping_cart,
+                          theme.colorScheme.secondary,
+                          percentage: '13%',
+                          isIncrease: true,
                         ),
-                        const SizedBox(height: 16),
+                        _buildStatCard(
+                          context,
+                          'Feedback',
+                          state.feedbackCount.toString(),
+                          Icons.thumb_up,
+                          theme.colorScheme.tertiary,
+                          percentage: '13%',
+                          isIncrease: false,
+                        ),
+                        _buildStatCard(
+                          context,
+                          'Reviews',
+                          '2.6k',
+                          Icons.reviews,
+                          theme.colorScheme.error,
+                          percentage: '1.2%',
+                          isIncrease: true,
+                          extraInfo: '233 ASINs',
+                        ),
                       ],
+                    );
+                  } else {
+                    return Center(child: Text('No data available.'));
+                  }
+                },
+              ),
+            ),
+            const SizedBox(height: 16),
+            // My Workflow Card (unchanged)
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: theme.colorScheme.surface,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: theme.colorScheme.shadow,
+                    blurRadius: 8,
+                    offset: const Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.bar_chart, color: theme.colorScheme.secondary),
+                      const SizedBox(width: 8),
+                      Text(
+                        'My Workflow',
+                        style: theme.textTheme.bodyMedium?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    "You don’t have any Workflows running. Create a Workflow in order to view its insights here.",
+                    style: theme.textTheme.bodyMedium?.copyWith(
+                      color: theme.colorScheme.onSurface.withOpacity(0.7),
                     ),
-                  );
-                }
-                return Container();
-              },
+                  ),
+                  SizedBox(
+                    height: 70,
+                    child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/send.png')
+                      ]
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Handle button press
+                      },
+                      child: const Text('Create a Workflow'),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildStatCard(
+    BuildContext context,
+    String title,
+    String value,
+    IconData icon,
+    Color color, {
+    String? percentage,
+    bool? isIncrease,
+    String? extraInfo,
+  }) {
+    final theme = Theme.of(context);
+
+    return Container(
+      decoration: BoxDecoration(
+        color: theme.colorScheme.surface,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: theme.colorScheme.shadow,
+            blurRadius: 8,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Icon(icon, color: color),
+              const SizedBox(width: 8),
+              Text(
+                title,
+                style: theme.textTheme.bodyMedium?.copyWith(
+                  color: theme.colorScheme.onSurface.withOpacity(0.7),
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+          Text(
+            value,
+            style: theme.textTheme.displayLarge?.copyWith(
+              fontSize: 24,
+            ),
+          ),
+          if (percentage != null)
+            Row(
+              children: [
+                Icon(
+                  isIncrease == true
+                      ? Icons.arrow_upward
+                      : Icons.arrow_downward,
+                  color: isIncrease == true
+                      ? theme.colorScheme.primary
+                      : theme.colorScheme.error,
+                  size: 16,
+                ),
+                const SizedBox(width: 4),
+                Text(
+                  percentage,
+                  style: TextStyle(
+                    color: isIncrease == true
+                        ? theme.colorScheme.primary
+                        : theme.colorScheme.error,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          if (extraInfo != null)
+            Text(
+              extraInfo,
+              style: theme.textTheme.bodyMedium?.copyWith(
+                fontSize: 12,
+                color: theme.colorScheme.onSurface.withOpacity(0.6),
+              ),
+            ),
+        ],
       ),
     );
   }
